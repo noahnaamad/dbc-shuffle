4aa1edb4317e0b187947b721c8f88ca542aeeacb
Mike Laurel <mlaurel@gmail.com>
2016-06-27 15:30:07 -0700

    update heroku postgresql setup

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20160617174929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-  end
-
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
