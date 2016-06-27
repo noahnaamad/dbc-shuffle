65859381cf44f13c3b1e8ac1d2815ba163fde020
Mike Laurel <mlaurel@gmail.com>
2016-06-27 15:58:50 -0700

    heroku migrate testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617174929) do
+ActiveRecord::Schema.define(version: 20140707111715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
