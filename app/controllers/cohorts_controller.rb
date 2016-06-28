dbbb391d42eb71228cd411827fbc947554982756
Brad Harris <email": null>
2016-06-27 18:39:32 -0700

    building crud cohorts

---
+++ app/controllers/cohorts_controller.rb
@@ -0,0 +1,31 @@
+class CohortsController < ApplicationController
+  def index
+    @cohorts = Cohort.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+end
