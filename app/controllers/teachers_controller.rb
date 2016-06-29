8a9c6cc830d89ebfcd8b8c919642b71a1c80b9b0
Mike Laurel <mlaurel@gmail.com>
2016-06-28 22:37:28 -0700

    add teacher form working

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,4 +4,18 @@ class TeachersController < ActionController::Base
     @teacher = current_teacher
   end
 
+  def create
+    puts "============="
+    puts teacher_params
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+      flash[:notice] = 'Teacher added!'
+      redirect_to teachers_path
+    else
+      render 'new'
+    end
+
+  end
+
 end
