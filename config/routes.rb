de8b5a0757036dc9c75b3d93658f296f6e4fd5c5
Mike Laurel <mlaurel@gmail.com>
2016-06-28 18:44:11 -0700

    add teacher folder and admin view to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
 
   resources 'cohorts'
 
+  namespace :admin do
+    resources :teachers, only: [:index, :create, :destroy]
+  end
+
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
