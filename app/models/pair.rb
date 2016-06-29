e04ce0b835c47ba3ecbad7294fa7499f4f7ae7fa
Noah Naamad <email": null>
2016-06-28 17:32:20 -0700

    first draft of pair create override

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,13 @@
 class Pair < ActiveRecord::Base
   belongs_to :stud1, {foreign_key: :stud1_id, class_name: "Student"}
   belongs_to :stud2, {foreign_key: :stud2_id, class_name: "Student"}
+
+  def create(stud1, stud2)
+  	if stud2 < stud1
+  		super(stud1_id: stud2, stud2_id: stud1)
+  	else
+  		super(stud1_id: stud1, stud2_id: stud2)
+  	end
+  end
+
 end
