6c9a7ec40eac5b63522e05d57f3f6e64256c0fa9
Maeve Tierney <maevet27@gmail.com>
2016-06-29 11:10:50 -0700

    Did nothing but move little things in css

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,22 +3,4 @@ class TeachersController < ApplicationController
   def show
     @cohorts_in_location = current_teacher.location.cohorts
   end
-
-<<<<<<< HEAD
-=======
-  def create
-    puts "============="
-    puts teacher_params
-    @teacher = Teacher.new(teacher_params)
-
-    if @teacher.save
-      flash[:notice] = 'Teacher added!'
-      redirect_to teachers_path
-    else
-      render 'new'
-    end
-
-  end
->>>>>>> b0d8f05751eb2b04815fd6ae3953e11e9758c70c
-
 end
